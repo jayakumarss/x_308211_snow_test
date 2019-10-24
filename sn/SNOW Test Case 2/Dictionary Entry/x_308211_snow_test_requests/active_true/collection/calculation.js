f2d2439fd4d2487865922f2b46d330bdf44568af
System Administrator <admin@example.com>
2019-10-24 10:17:02 -0400

    Changes by 'System Administrator' on 'SNOW Test Case 2 - 1.0.0' -- Build #1

---
+++ sn/SNOW Test Case 2/Dictionary Entry/x_308211_snow_test_requests/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : SNOW Test Case 2
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-10-24 14:13:10
+ * Created By  : admin
+ * Updated On  : 2019-10-24 14:13:10
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=cd6fcbf3db200010343f58b3ca96196f
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
